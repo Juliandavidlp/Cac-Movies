25a4007a1d429ef7798973c37a2aec84d1f759df
Julián <juliandorao@gmail.com>
2024-04-18 10:57:15 -0300

    Codo a codo 4.0

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,17 @@
 console.log("Buen día mundo desde la consola!");
+
+// Comentario de una línea
+
+/* 
+    Comentario de 
+    varias líneas
+    ~
+*/
+
+//Declaración de variables
+var movieOne; //Una variable global, legacy
+
+let movieTwo;
+
+const YEAR= 2024//Para declarar constantes, en mayúsculas.
+
