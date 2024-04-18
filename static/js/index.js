e03ae9929cd1619ae462a978d6c3bdd4c13c6861
Julián <juliandorao@gmail.com>
2024-04-18 11:03:33 -0300

    Codo a codo 4.0

--- static/js/index.js
+++ static/js/index.js
@@ -11,7 +11,14 @@ console.log("Buen día mundo desde la consola!");
 //Declaración de variables
 var movieOne; //Una variable global, legacy
 
-let movieTwo;
+//Tipos de datos, declaración y definición
+let movie2= 'El padrino';
+let rating= 8; //number - integer
+let rating2=9.7; //number - float
+let rating3= "6.5";
+let is New= false; // boolean o lógico
+
+console.log(typeof(rating3));
 
 const YEAR= 2024//Para declarar constantes, en mayúsculas.
 
