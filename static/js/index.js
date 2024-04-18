309e8b98ff2892c6d1db8fe88dec044531508d82
Julián <juliandorao@gmail.com>
2024-04-18 11:33:02 -0300

    Codo a codo 4.0

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+//Imprimir en la consola:
 console.log("Buen día mundo desde la consola!");
 
 // Comentario de una línea
@@ -17,8 +18,31 @@ let rating= 8; //number - integer
 let rating2=9.7; //number - float
 let rating3= "6.5";
 let isNew= false; // boolean o lógico
+let movie3=null // objet, para asignarle un string o un objeto, por ejemplo
+
+//Una propiedad y un valor, para definir un objeto
+let movie4={ 
+    name:'El padrino 3'
+}
 
 console.log(typeof(rating3));
 
 const YEAR= 2024//Para declarar constantes, en mayúsculas.
+
+// Operaciones aritméticas
+let average= (rating+rating2 + parseInt(rating3)) / 2;
+
+// Operaciones de concatenación
+console.log('El promedio es:'+ average) //Ojo, porque si no cambio el tipo de dato concatena números con texto
+
+
+
+// Manipulación del DOM: Document Objet Model
+
+//Capturo un elemento desde Javascript
+let parrafo= document.querySelector("#parrafo");
+
+console.log(parrafo);
+
+parrafo.innerHTML= 'Hola, lo agrego desde JS.';
 
---
+++ static/js/validation-register.js
@@ -0,0 +1,7 @@
+let btnSend = document.querySelector("#btn-send");
+
+btnSend.addEventListener('click',function(){
+    let inputFirstname = document.querySelector("#firstname");
+    alert('Enviando datos...'+inputFirstname.value);
+    //Accedo al valor que una persona ingresó en un input.
+});
